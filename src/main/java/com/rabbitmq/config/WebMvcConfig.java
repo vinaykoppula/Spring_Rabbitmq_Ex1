b1ff1577d05516a7b396fc864cc9907dcd1f2b98
vinay <vinay@LAPTOP-GH7PUIOE>
2022-02-18 17:41:54 -0600

    added comment...

--- src/main/java/com/rabbitmq/config/WebMvcConfig.java
+++ src/main/java/com/rabbitmq/config/WebMvcConfig.java
@@ -68,7 +68,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
 	@Override
 	public void configureAsyncSupport(AsyncSupportConfigurer configurer) {
-		configurer.setDefaultTimeout(3000);
+		configurer.setDefaultTimeout(3000);//added by ajay
 		configurer.registerCallableInterceptors(new TimeoutCallableProcessingInterceptor());
 	}
 
